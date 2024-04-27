f544f3c6f5a960bf871a8b6d8f81fea1eeb19ae0
Rin <164251452+linchannn@users.noreply.github.com>
2024-04-27 05:50:13 +0000

    add components

---
+++ vue3/src/components/DashboardPannel.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+h2 oooo
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ vue3/src/components/DashboardTable.vue
@@ -0,0 +1,8 @@
+<template lang="pug">
+h2 table
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped></style>
