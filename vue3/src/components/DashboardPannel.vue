210d7859fda494fc40a07b6549873aef72ac13e2
Rin <164251452+linchannn@users.noreply.github.com>
2024-04-28 12:13:14 +0000

    added some parts

--- vue3/src/components/DashboardPannel.vue
+++ vue3/src/components/DashboardPannel.vue
@@ -1,7 +1,32 @@
 <template lang="pug">
-h2 oooo
+v-container(fluid)
+  div.dashboard-pannel
+    v-row
+      v-col
+        v-card(color="white")
+          p テナントID
+      v-col(cols="9", align="right")
+        v-card(color="white")
+          p aaa
+    v-row
+      v-col
+        v-card(color="white")
+          p 質問回数
+      v-col
+        v-card(color="white")
+          p 新規登録ファイル数
+      v-col
+        v-card(color="white")
+          p 新規登録ファイル容量
+      v-col
+        v-card(color: "white")
+          p ストレージ容量
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.dashboard-pannel {
+  background-color: gray;
+}
+</style>
--- vue3/src/components/TheWelcome.vue
+++ vue3/src/components/TheWelcome.vue
@@ -1,8 +1,31 @@
 <script setup lang="ts">
+import {ref} from 'vue'
 import { VBtn } from 'vuetify/components/VBtn'
+import DashboardPannel from './DashboardPannel.vue'
+import DashboardTable from './DashboardTable.vue'
+
+const dt = ref<string>('2024/4/26 16:50')
+
+
+const fetch = () => {
+  window.alert("ふぇっ")
+}
 </script>
 
 <template lang="pug">
-v-btn ぱぐ
+v-container(fluid)
+  v-row
+    v-col
+      h1 だんだんだーん
+    v-col
+      h3 {{ dt }} &nbsp; 現在
+    v-col
+      v-btn(depressed, @click="fetch()") 取得
+  v-row
+    v-col(cols="7")
+      dashboard-pannel
+  v-row
+    v-col(cols="7")
+      dashboard-table
 </template>
 <style scoped></style>
