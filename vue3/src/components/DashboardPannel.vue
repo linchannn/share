f2459c9a7e3f28e9490d87cc08795f2cfdc0a964
Rin <164251452+linchannn@users.noreply.github.com>
2024-04-30 16:08:09 +0000

    セレクトボックス導入

--- vue3/src/components/DashboardPannel.vue
+++ vue3/src/components/DashboardPannel.vue
@@ -1,48 +1,34 @@
 <template lang="pug">
-v-container(fluid)
-  div.dashboard-pannel
-    v-row
-      v-col
-        v-card.pa-4(outlined)
-          v-card-title.subtitle-1 テナントID
-          v-card-text
-            v-label.body-1 sample
-      v-col(cols="9", align="right")
-        v-card
-          p aaa
-    v-row
-      v-col
-        v-card
-          p 質問回数
-      v-col
-        v-card
-          p 新規登録ファイル数
-      v-col
-        v-card
-          p 新規登録ファイル容量
-      v-col
-        v-card
-          p ストレージ容量
+div.card
+  p テナントID
+  p gg
+div.card
+  span 期間
+  select(v-model="selected")
+    option(v-for="item in items" :value="item.value") {{ item.text }}
+div.card
+  p 質問回数
+div.card
+  p 新規登録ファイル数
+div.card
+  p 新規登録ファイル容量
+div.card
+  p ストレージ容量
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+const selected = ref()
+const items = [
+  // セレクトボックスのオプション
+  { value: 'option1', text: 'オプション1' },
+  { value: 'option2', text: 'オプション2' }
+  // 他のオプション...
+]
+</script>
 
 <style scoped>
-.dashboard-pannel {
-  background-color: gray;
-}
-/* 背景色を白に設定 */
-.v-card {
-  background-color: white;
-}
-
-/* カードタイトルのフォントサイズを小さく設定 */
-.v-card-title {
-  font-size: 0.875rem; /* 14px */
-}
-
-/* カードコンテンツのフォントサイズを通常に設定 */
-.v-label {
-  font-size: 1rem; /* 16px */
+.card {
+  border: solid;
 }
 </style>
