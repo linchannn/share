fdb48f35837da1e05ea13d40a7b16c6256c46ebf
Rin <164251452+linchannn@users.noreply.github.com>
2024-04-30 17:10:33 +0000

    variables

--- vue3/src/components/DashboardPannel.vue
+++ vue3/src/components/DashboardPannel.vue
@@ -1,23 +1,29 @@
 <template lang="pug">
 div.card
-  p テナントID
-  p gg
+  p テナントID: &nbsp; {{ tenantId }}
 div.card
   span 期間
   select(v-model="selected")
     option(v-for="item in items" :value="item.value") {{ item.text }}
 div.card
-  p 質問回数
+  p 質問回数: &nbsp; {{ queryCount }}
 div.card
-  p 新規登録ファイル数
+  p 新規登録ファイル数: &nbsp; {{ newFilesCount }}
 div.card
-  p 新規登録ファイル容量
+  p 新規登録ファイル容量: &nbsp; {{ newFilesSize }}
 div.card
-  p ストレージ容量
+  p ストレージ容量: &nbsp; {{ totalStorageCapacity }}
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+
+const tenantId = ref<string>('12345')
+const queryCount = ref<number>(0)
+const newFilesCount = ref<number>(0)
+const newFilesSize = ref<number>(0)
+const totalStorageCapacity = ref<number>(0)
+
 const selected = ref()
 const items = [
   // セレクトボックスのオプション
